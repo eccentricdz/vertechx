09d915794d2292b7cc2e1ef5b6fb1a794c7ab250
Rahul Agarwal <simplyrahul93@gmail.com>
2015-03-31 19:35:47 +0530

    contacts and registration added, backend missing

--- vertechx.js
+++ vertechx.js
@@ -1,7 +1,43 @@
-
+var contacts = [
+{
+	name : 'Jay Megotia',
+	mobile : '8609491889',
+	email : 'jaykumarmegotia@gmail.com'
+},
+{
+	name : 'Sharad Agrawal',
+	mobile : '7549014303',
+	email : 'sharadagrawal.bit@gmail.com'
+},
+{
+	name : 'Rahul Agarwal',
+	mobile : '8797593485',
+	email : 'simplyrahul93@gmail.com'
+},
+{
+		name : 'Nimesh Ghelani',
+	mobile : '7549157225',
+	email : 'nimeshghelani@gmail.com'
+},
+{
+		name : 'Richa Varma',
+	mobile : '8292505450',
+	email : 'richavarma.0902@gmail.com'
+},
+{
+	name : 'Swapnil Jha',
+	mobile : '9572227745',
+	email : ''
+},
+{
+	name : 'Harsh Vardhan',
+	mobile : '9534163202',
+	email : 'harsh.vardhan1071@gmail.com'
+}
+];
 var Typer={
 	name : 'vertechx',
-	text: '<span id="a">vertechx</span>:<span id="b">~</span><span id="c">$</span> Vertechx 2015<br/><br/>Welcome to Vertechx 2015<!-- laglaglaglaglaglaglaglaglaglaglaglag --><p>The annual technology festival of <a href="www.bitmesra.ac.in" target="_blank">Birla Institute of Technology, Mesra</a><br/><br/></p><!-- qowifjqwoeiijefoqwijefoijfqiwoefjj --><p>Follow us on our <a href="https://www.facebook.com/vertechxbitmesra" target="_blank">facebook</a> page<br/><br/></p><!- oqwipjefqwioefjwioqfjoiqwjfeioqwjefoi --><p>The details are coming very soon. Stay tuned for updates.<br/><br/></p><!-- owlsqweoifjqwoefijqwoeifjqwoefijwef -->If you would like to get in touch with us<!-- slightdelayhere-->, mail us at : <a href="mailto:vertechx.bitmesra@gmail.com">vertechx.bitmesra@gmail.com</a><p>Have a good day :)</p>',
+	text: '<span id="a">vertechx</span>:<span id="b">~</span><span id="c">$</span> Vertechx 2015<br/><br/>Welcome to Vertechx 2015<!-- laglaglaglaglaglaglag--><p>The annual technology festival of <a href="www.bitmesra.ac.in" target="_blank">Birla Institute of Technology, Mesra</a><br/><br/></p><!-- qowifjqwoeiijefoqwioefjj --><p>Follow us on our <a href="https://www.facebook.com/vertechxbitmesra" target="_blank">facebook</a> page<br/><br/></p><!- oqwipjefqwioefjwioqwji --><p>The event details are coming very soon. Stay tuned for updates.<br/></p>',
 	index:0, 
 	speed:2, 
 	file:"", 
@@ -77,8 +113,12 @@ function replaceUrls(text) {
 }
 }
 
-Typer.speed=3;
+Typer.speed=15;
 Typer.file="vertechx.txt";
+
+Typer.text+='If you would like to get in touch with us<!-- slightdelayhere-->, mail us at : <a href="mailto:vertechx.bitmesra@gmail.com">vertechx.bitmesra@gmail.com</a>';
+Typer.text+="<br />or type <span id='a'>'contact'</span> to get the contact details";
+Typer.text+="<br /><br />Type <span id='a'>'register'</span> to register your team now"
 Typer.init();
  
 
@@ -98,17 +138,41 @@ $(document).ready(function() {
 
 	var beep = document.getElementById('beep');
 
-	function alertify(msg){
-		beep.play();
-		Typer.write('<p class="alert">'+msg+'</p>');
-		
+	function printContact(){
+		for(var i=0;i<contacts.length;i++)
+			{
+				Typer.write('<p class="contacts">'+contacts[i].name+' : '+contacts[i].mobile+' / <a href="mailto:'+contacts[i].email+'">'+contacts[i].email+'</a></p>')
+			}
+	}
+
+	function alertify(msg, success){
+		if(!success)
+		{
+			beep.play();
+			Typer.write('<p class="alert">'+msg+'</p>');
+		}
+		else
+			Typer.write('<p class="success">'+msg+'</p>');
 	}
 
 	function processCommand(cmd){
 		cmd = cmd.split(' ');
 
-		console.log(cmd);
-		alertify("\'"+cmd+"' is not recognized as an internal or external command, stay tuned, we will update the list of valid commands");
+		if(cmd[0]=='contact')
+		{
+			printContact();
+		}
+
+		if(cmd[0]=='register')
+		{
+			window.open(
+					'register.html',
+					'_blank' // <- This is what makes it open in a new window.
+					);
+		}
+
+		else
+		alertify("\'"+cmd+"' is not recognized as an internal or external command, stay tuned, we will update the list of valid commands", false);
 	}
 
 $(document).click(function(){
